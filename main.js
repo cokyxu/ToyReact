4765acfe1da24f428f64a4339d92e9977d3e6d26
cokyxu <913485800@qq.com>
2020-07-21 22:12:34 +0800

    第二次提交

--- main.js
+++ main.js
@@ -1,22 +1,57 @@
 import { ToyReact, Component } from "./ToyReact"
 
-class MyComponent extends Component {
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        }
+    }
+
     render() {
-        return <div>
-            <div>cool</div>
+        return (
+            <button className="square" onClick={() => { this.setState({value: 'X' }); console.log('my click!!!') }}>
+                {this.state.value ? this.state.value : ""}
+            </button>
+        )
+    }
+}
+
+class Board extends Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={i}
+            />
+        );
+    }
+
+    render() {
+        return (
             <div>
-                Hello{this.children}
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
             </div>
-            <div>World</div>
-        </div>
+        );
     }
 }
 
 
-let component = <MyComponent  id="a" class="b">
-<div>123</div>
-<div>我最帅！！！！！！！！！</div>
-</MyComponent>
+let component = <Board id="a" class="b">
+</Board>
 
 ToyReact.render(component, document.body);
 
